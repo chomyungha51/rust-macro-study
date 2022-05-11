681dbd23127676afbb5999e3d036ff70116520b1
chomyungha <chomyungha@gmail.com>
2022-05-11 10:45:42 +0900

    시그니처 조사

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
+/*
+MacroFagSpec
+
+block : {...}
+expr : expression
+ident : variable or function name
+item : module, function, typeAlias, struct, enum, union, trait, staticItem...
+stmt : statement
+tt : token tree
+ty : type
+vis : pub
+...
+
+MacroRepOp
+* : any number of repetition
++ : any number but at least one
+? : optional at most one
+
+*/
+
+#[macro_export]
+macro_rules! custom_println_macro {
+    ($arg:int) => { println!("{}", $arg);};
+    (expr) => (expr);
+}
+
+#[macro_export]
+macro_rules! custom_vec_macro {
+    () => ();
+    () => ();
+    () => ();
+}
+
+#[macro_export]
+macro_rules! to_str {
+    () => ();
+    () => ();
+}
+
 fn main() {
-    println!("Hello, world!");
-}
 
-#[macro_export]
-macro_rules! custom_println_macro() {
-
-}
-
-#[macro_export]
-macro_rules! custom_vec_macro() {
-
+    custom_println_macro!();
+    custom_vec_macro!();
+    to_str!();
 }
